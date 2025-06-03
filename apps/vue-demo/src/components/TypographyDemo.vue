9aafa3a9786cc2759970bb159ea80ceaf589112a
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2025-06-04 00:11:31 +0200

    add all elements html to svelte and vue apps

---
+++ apps/vue-demo/src/components/TypographyDemo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Typography</h1>
+    <h1>h1 <small>small h1</small></h1>
+    <h2>h2 <small>small h2</small></h2>
+    <h3>h3 <small>small h3</small></h3>
+    <h4>h4 <small>small h4</small></h4>
+    <h5>h5 <small>small h5</small></h5>
+    <h6>h6 <small>small h6</small></h6>
+    <hr />
+  </div>
+</template> 
--- apps/vue-demo/src/main.ts
+++ apps/vue-demo/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import "chimeracss/build/chimera.css"
 import App from './App.vue'
+
+// Add ChimeraCSS stylesheet
+const stylesheet = document.createElement('link')
+stylesheet.id = 'stylesheet'
+stylesheet.rel = 'stylesheet'
+stylesheet.type = 'text/css'
+stylesheet.href = 'https://unpkg.com/chimeracss/build/chimera.css'
+document.head.appendChild(stylesheet)
 
 createApp(App).mount('#app')
