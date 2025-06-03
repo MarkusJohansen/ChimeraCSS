e45cbd9695250a63df48547baadbd7ab82dcda86
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2025-06-03 23:55:43 +0200

    initial react, svelte and vue apps

---
+++ apps/vue-demo/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="app">
+    <h1>Vue Counter Demo</h1>
+    <div class="card">
+      <button @click="count++">
+        Count is {{ count }}
+      </button>
+    </div>
+  </div>
+</template>
+
+
